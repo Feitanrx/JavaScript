389caa174e498784cd6c4268c008dcfe5840836f
João Marcos Ferreira <joao_jmpf77@hotmail.com>
2025-01-31 12:25:15 -0300

    Olá mundo

---
+++ Alura/imersão/ex001js/app.js
@@ -0,0 +1,12 @@
+alert('Boas vindas ao nosso site!');
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+let nome = prompt ('Digite seu Nome');
+let idade = prompt ('Digite sua idade');
+if (idade >= 18) {
+    alert('Você pode tirar habilitação');
+}
